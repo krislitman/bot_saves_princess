2928c52fdd9047c3c6d9e0e8b9cf69b6761d1130
Kris Litman <kris.d.litman@gmail.com>
2021-06-24 21:38:44 -0400

    Can find princess & mario

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,16 +1,17 @@
 class Bot
   def self.displayPathtoPrincess(int, grid)
-    princess = find_princess(grid)
+    princess = find_location(grid, "p")
+    mario = find_location(grid, "m")
     require 'pry'; binding.pry
   end
   
-  def self.find_princess(grid)
-    princess = nil
+  def self.find_location(grid, chr)
+    found = nil
     grid.each_with_index do |ind, elem|
-      if ind.include? "p"
+      if ind.include? chr
         ind.split("").each_with_index do |inde, eleme|
-          if inde == "p"
-            princess = [elem, eleme]
+          if inde == chr
+            found = [elem, eleme]
           else
             next
           end
@@ -19,6 +20,6 @@ class Bot
         next
       end
     end
-    return princess
+    return found
   end
 end
--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -5,9 +5,19 @@ RSpec.describe 'Bot' do
     bot = Bot.new
     expect(bot).to be_an_instance_of(Bot)
   end
-  it 'Has a function #displayPathtoPrincess that takes #N and #arraygrid' do
+  xit 'Has a function #displayPathtoPrincess that takes #N and #arraygrid' do
     array_grid = ["---", "-m-", "p--"]
     expected = Bot.displayPathtoPrincess(3, array_grid)
     expect(expected).to eq("DOWN\nLEFT")
+  end
+  it 'Can find the princess location' do
+    array_grid = ["---", "-m-", "p--"]
+    expected = Bot.find_location(array_grid, "p")
+    expect(expected).to eq([2, 0])
+  end
+  it 'Can find mario location' do
+    array_grid = ["---", "-m-", "p--"]
+    expected = Bot.find_location(array_grid, "m")
+    expect(expected).to eq([1, 1])
   end
 end
