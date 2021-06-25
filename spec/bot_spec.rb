1e439e3286d6ca7aaac4514417677d6ad0f75e05
Kris Litman <kris.d.litman@gmail.com>
2021-06-24 21:44:17 -0400

    Added larger grid tests for bot

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -1,6 +1,6 @@
 require './lib/bot.rb'
 
-RSpec.describe 'Bot' do
+RSpec.describe 'Bot Spec' do
   it 'Bot object can be created' do
     bot = Bot.new
     expect(bot).to be_an_instance_of(Bot)
@@ -14,6 +14,16 @@ RSpec.describe 'Bot' do
     array_grid = ["---", "-m-", "p--"]
     expected = Bot.find_location(array_grid, "p")
     expect(expected).to eq([2, 0])
+  end
+  it 'Can find the princess location in larger grid' do
+    array_grid = ["-----", "--m--", "p----"]
+    expected = Bot.find_location(array_grid, "p")
+    expect(expected).to eq([2, 0])
+  end
+  it 'Can find the princess location in an even larger grid' do
+    array_grid = ["-----", "-----", "--m--", "-----", "----p"]
+    expected = Bot.find_location(array_grid, "p")
+    expect(expected).to eq([4, 4])
   end
   it 'Can find mario location' do
     array_grid = ["---", "-m-", "p--"]
